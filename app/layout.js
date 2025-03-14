6af0e3b950a36a37494b56426f57bfefa7c9ee5f
maryumikram <maryumikram98@gmail.com>
2025-03-14 22:05:32 +0500

    Added Work Experience Tracker & Calculator project

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Work Experience Tracker & Calculator - Next.js & React",
+  description:
+    "Track and manage your work experience effortlessly with this Next.js and React-powered Work Experience Tracker. Add job details, calculate total experience, and maintain a professional career timeline.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import ExperienceCalculator from "./components/ExperienceCalculator";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl text-gray-800  font-medium text-center  my-4">
+        Work Experience Tracker and Calculator
+      </h1>
+      <ExperienceCalculator />
+    </>
+  );
+}
